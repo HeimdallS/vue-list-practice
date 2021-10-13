b7069d92687011e205fa28115d70ba1e346867f0
Heimdall Sham <heim.sham@icloud.com>
2021-10-13 15:10:04 -0400

    Created add task component. Created addTask functionality.

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import {library} from '@fortawesome/fontawesome-svg-core';
 import {fas} from '@fortawesome/free-solid-svg-icons';
-import App from './App.vue'
+import App from './App.vue';
 
 library.add(fas);
 
