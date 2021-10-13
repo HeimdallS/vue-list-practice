73529a38537f68b3ff7e1e63c31c96ff2eb95e71
Heimdall Sham <heim.sham@icloud.com>
2021-10-12 21:44:13 -0400

    Applied delete functionality.

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,7 +1,7 @@
 <template>
 <div :class="[task.reminder ? 'reminder' : '', 'task']">
     <h3>{{task.text}}
-        <fa icon="times" class="icon"/>
+        <fa @click="onDelete(task.id)" icon="times" class="icon"/>
     </h3>
     <p>{{task.day}}</p>
 </div>
@@ -12,6 +12,11 @@ export default {
     name: 'Task',
     props: {
         task: Object,
+    },
+    methods: {
+        onDelete(id) {
+            this.$emit('delete-task', id)
+        }
     }
 }
 </script>
